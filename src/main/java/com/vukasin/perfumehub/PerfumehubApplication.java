eeaa894fc91991a1328d8cd61cb158fb41bc0fd8
Vukasin Sevic <vukasin.sevic@gmail.com>
2026-07-24 18:48:37 +0200

    Initial commit

---
+++ src/main/java/com/vukasin/perfumehub/PerfumehubApplication.java
@@ -0,0 +1,13 @@
+package com.vukasin.perfumehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfumehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfumehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vukasin/perfumehub/PerfumehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vukasin.perfumehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfumehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
